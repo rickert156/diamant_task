110ca14d761877a6d72c78a24907dae5f8c9e741
rickert156 <rickert156@rambler.ru>
2025-12-10 08:20:12 +0400

    update progress

--- telegram_private_chat.py
+++ telegram_private_chat.py
@@ -1,31 +1,38 @@
 import os
 import sys
+import telegram
+from config import TOKEN_BOT, CHAT_ID
 from check_mx import divide_line
+
+def send_text(message_text:str):
+    bot = telegram.Bot(token=TOKEN_BOT)
+    bot.send_message(chat_id=CHAT_ID, text=message_text)
 
 def read_file(file_name:str) -> str | None:
     text = None
     try:
-        line = divide_line()
         with open(file_name, 'r') as file:
             text = file.read().strip()
-        print(
-                f'\nТекст файла {file_name}\n'
-                f'{line}\n'
-                f'{text}\n'
-                f'{line}'
-            )
     except Exception as err:
         print(f'Error: {err}')
 
     return text
 
 def main():
+    line = divide_line()
     params = sys.argv
     if len(params) == 2 and '.txt' in params[1]:
         if os.path.exists(params[1].strip()):
             file_name = params[1].strip()
             print(f'Файл {file_name} обнаружен')
-            read_file(file_name=file_name)
+            text = read_file(file_name=file_name)
+            print(
+                    f'\nТекст файла {file_name}\n'
+                    f'{line}\n'
+                    f'{text}\n'
+                    f'{line}'
+            )
+            send_text(message_text=text)
         else:
             print(f'Файл {params[1].strip()} не обнаружен')
         
